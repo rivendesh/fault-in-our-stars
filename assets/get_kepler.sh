8cfda749e5c29fcae0fb032bbbda964909249440
Desh Iyer <desh.iyer@gmail.com>
2026-04-22 19:14:29 -0700

    Create: Add scripts to download light curves

---
+++ assets/get_kepler.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+curl -f -R -o kplr007730747-2010296114515_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007730747/kplr007730747-2010296114515_slc.fits
+curl -f -R -o kplr007730747-2010326094124_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007730747/kplr007730747-2010326094124_slc.fits
+curl -f -R -o kplr007730747-2010355172524_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007730747/kplr007730747-2010355172524_slc.fits
+curl -f -R -o kplr007730747-2011024051157_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007730747/kplr007730747-2011024051157_slc.fits
+curl -f -R -o kplr007730747-2011053090032_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007730747/kplr007730747-2011053090032_slc.fits
+curl -f -R -o kplr007730747-2011073133259_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007730747/kplr007730747-2011073133259_slc.fits
+curl -f -R -o kplr007748238-2010296114515_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007748238/kplr007748238-2010296114515_slc.fits
+curl -f -R -o kplr007748238-2010326094124_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007748238/kplr007748238-2010326094124_slc.fits
+curl -f -R -o kplr007748238-2010355172524_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007748238/kplr007748238-2010355172524_slc.fits
+curl -f -R -o kplr007748238-2011024051157_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007748238/kplr007748238-2011024051157_slc.fits
+curl -f -R -o kplr007748238-2011053090032_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007748238/kplr007748238-2011053090032_slc.fits
+curl -f -R -o kplr007748238-2011073133259_slc.fits http://archive.stsci.edu/missions/kepler/lightcurves/0077/007748238/kplr007748238-2011073133259_slc.fits
+echo
+echo Script completed.  Note: Some \"Error 404\" messages are expected, depending on your parameters.  It is always recommended to confirm the expected number of files are successfully downloaded.
